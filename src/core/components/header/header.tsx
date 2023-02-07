00cec8f0875849012d651082d258d2bf67da6401
rocioRichi <paragit@gmail.com>
2023-02-07 20:58:17 +0100

    Same commit as before, something wrong

--- src/core/components/header/header.tsx
+++ src/core/components/header/header.tsx
@@ -1,22 +1,20 @@
-import logo from "./logo.svg";
-
+import fotoformulario from "../header/fotoformulario.png";
 type HeaderProps = { children: JSX.Element };
 
 export function Header({ children }: HeaderProps) {
   return (
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
+      <img src={fotoformulario} className="App-logo" alt="logo" />
+      <p>FORMULARIO</p>
+
+      {/* <a
         className="App-link"
         href="https://reactjs.org"
         target="_blank"
         rel="noopener noreferrer"
       >
         Learn React
-      </a>
+      </a> */}
       {children}
     </header>
   );
