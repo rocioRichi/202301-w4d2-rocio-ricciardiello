07d150bffee70d44afa410a6c5be051b849a269b
rocioRichi <paragit@gmail.com>
2023-02-08 10:11:52 +0100

    add form tag to Form1-2

--- src/features/components/form1/form1.tsx
+++ src/features/components/form1/form1.tsx
@@ -10,42 +10,44 @@ function Form1({ onNotify }: ChildrenProp) {
 
   return (
     <>
-      <fieldset>
-        <legend> Personal Data</legend>
-        <div>
-          <label>Name</label>
-          <input type="text"></input>
-        </div>
-        <div>
-          <label>Surname</label>
-          <input type="text"></input>
-        </div>
+      <form>
+        <fieldset>
+          <legend> Personal Data</legend>
+          <div>
+            <label>Name</label>
+            <input type="text"></input>
+          </div>
+          <div>
+            <label>Surname</label>
+            <input type="text"></input>
+          </div>
 
-        <div>
-          <label>DOB</label>
-          <input type="date"></input>
-        </div>
+          <div>
+            <label>DOB</label>
+            <input type="date"></input>
+          </div>
 
-        <div>
-          <label>Female</label>
-          <input type="radio" name="gender"></input>
-        </div>
+          <div>
+            <label>Female</label>
+            <input type="radio" name="gender"></input>
+          </div>
 
-        <div>
-          <label>Male</label>
-          <input type="radio" name="gender"></input>
-        </div>
+          <div>
+            <label>Male</label>
+            <input type="radio" name="gender"></input>
+          </div>
 
-        <div>
-          <label>I would rather not to answer</label>
-          <input type="radio" name="gender"></input>
-        </div>
+          <div>
+            <label>I would rather not to answer</label>
+            <input type="radio" name="gender"></input>
+          </div>
 
-        <div>
-          <label>¿Desea recibir información de nuestra newsletter?</label>
-          <input type="checkbox" name="n1"></input>
-        </div>
-      </fieldset>
+          <div>
+            <label>¿Desea recibir información de nuestra newsletter?</label>
+            <input type="checkbox" name="n1"></input>
+          </div>
+        </fieldset>
+      </form>
       <Button onClick={handleClick} />
     </>
   );
